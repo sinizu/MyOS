548142531e90b55859997e480a1bb02bead634c8
sinizu <1039187709@qq.com>
2024-01-16 22:08:13 +0800

    修改ignore规则

--- src/boot.asm
+++ src/boot.asm
@@ -18,6 +18,18 @@ mov ax, 0xb800
 mov ds, ax
 ; 显示一个H
 mov byte [0], 'H'
+mov byte [2], 'e'
+mov byte [4], 'l'
+mov byte [6], 'l'
+mov byte [8], 'o'
+mov byte [10], ','
+mov byte [12], 'w'
+mov byte [14], 'o'
+mov byte [16], 'r'
+mov byte [18], 'l'
+mov byte [20], 'd'
+mov byte [22], '!'
+
 
 ; 阻塞
 jmp $
